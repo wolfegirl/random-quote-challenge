bbcc3a70ff894dd198d59f1a0530cb1a5b61addc
Leslie <lesliejohnswolfe@gmail.com>
2017-07-16 16:27:13 -0600

    Tried to add the no repeat functionality - not so successful, but everything work

--- js/script.js
+++ js/script.js
@@ -2,39 +2,43 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 //variables for phase one global scope - meets expectations
-var random;
+var randomQuote;
 var message = '';
 var finalQuotePrint;
-//variables for phase two global scope- exceeds expectations - change background color with every new quote
+//variables for phase two global scope- exceeds expectations
 var red;
 var blue;
 var green;
-var rgbColor;
 var randomBackgrounColor;
 var intervalID = setInterval(printQuote, 30000); // change quote every 30 seconds
+var usedQuote = [];
+var removedQuotes;
 
 //function for getting the random quotes
 
 function getRandomQuote() {
-  random = Math.floor(Math.random() * quotes.length); //generate a random quote object.
-  return quotes[random]; //return my random quote object
-  }
+
+  randomQuote = (Math.floor(Math.random() * quotes.length)); //generate a random quote object.
+  //quotes.splice(randomQuote, 1); // take out the used quote from the quotes array.
+  //usedQuote.push(quotes[randomQuote]); // put the used quoted into the usedQuote array
+  return quotes[randomQuote]; //return my random quote object
+
+}
 
 //function to make a random hex color
 
 function changeBackgroundColor() {
-  randomBackgroundColor = Math.floor(Math.random() * 16777215).toString(16); //I researched and found this snippet because I thought I would need a hex number instead of applying the random rgb function that we learned.
+  randomBackgroundColor = Math.floor(Math.random() * 16777215).toString(16); //I researched and found this snippet because I thought I would need a hex number instead of applying the random rgb function that we learned. I did learn that either will work, but this is cleaner to me. See line 36.
   document.body.style.backgroundColor = '#' + randomBackgroundColor;
-
 }
 
-/* I can't get this method to work so I found the hex solution
+/* This is my other solution to the random background but I like the hex solution better.
 function changeBackgroundColor() {
 
 red = Math.floor(Math.random() * 255);
 green = Math.floor(Math.random() * 255);
-blue = Math.floor(Mathrandom() * 255);
-document.body.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+blue = Math.floor(Math.random() * 255);
+return document.body.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
 }
 
 */
@@ -42,13 +46,18 @@ document.body.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + 
 //function to print the quotes
 
 function printQuote() {
-  changeBackgroundColor();//This calls the random background color
-  finalQuotePrint = getRandomQuote(); //Calling the getRandomQuote function and storing it in a variable
-//displaying the message in the proper HTML  - this is behaving wonky. the source wraps to another line. Can't figure out why.
-  message = '<p class="quote">' + finalQuotePrint.quote + '</p>'; //using finalQuotePrint variable and adding the quote key to it with dot notation
-  message += '<p class="source">' + finalQuotePrint.source + '</p>';
-  message += '<span class="citation">' + finalQuotePrint.citation + '</span>';
-  message += '<span class="year">' + finalQuotePrint.year + '</span>';
-  message += '<span class="year">' + finalQuotePrint.tags + '</span>';
-  document.getElementById('quote-box').innerHTML = message;
-}
+
+    changeBackgroundColor();//This calls the random background color
+    finalQuotePrint = getRandomQuote(); //Calling the getRandomQuote function and storing it in a variable
+//displaying the message in the proper HTML
+    message = '<p class="quote">' + finalQuotePrint.quote + '</p>'; //using finalQuotePrint variable and adding the quote key to it with dot notation
+    message += '<p class="source">' + finalQuotePrint.source;
+    message += '<span class="citation">' + finalQuotePrint.citation + '</span>';
+    message += '<span class="year">' + finalQuotePrint.year + '</span>';
+    message += '<span class="tags">' + finalQuotePrint.tags + '</span></p>';
+    document.getElementById('quote-box').innerHTML = message;
+
+    console.log(finalQuotePrint.quote); //logging the used quote to the console
+
+  }
+  //puts all the objects back in the quotes array to start over
